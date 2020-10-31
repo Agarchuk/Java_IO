b892a223806509a38cc109f44f2484bf93fc0fbf
Harchuk Hanna <harchukhanna@gmail.com>
2020-10-31 16:50:16 +0300

    Создать и заполнить файл случайными целыми числами. Отсортировать содержимое файла по возрастанию.

---
+++ src/OptionalTasks/Task1/createFile.java
@@ -0,0 +1,34 @@
+package OptionalTasks.Task1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class createFile {
+    public static void main(String[] args) {
+        File dir = new File("data");
+        dir.mkdir();
+
+        try(FileWriter file = new FileWriter("data/File.txt")){
+            file.write(String.valueOf(sortArray(createArrayList(10))));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Integer> createArrayList(int count){
+        ArrayList<Integer> array = new ArrayList<Integer>(count);
+        for (int i = 0; i<count; i++){
+            array.add((int)(Math.random()*10));
+        }
+        return array;
+    }
+
+    public static ArrayList<Integer> sortArray(ArrayList<Integer> array){
+        Collections.sort(array);
+        return array;
+    }
+}
